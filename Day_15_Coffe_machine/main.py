326ac6d9741b3f09449491e4f6e5e603adfb8b54
faruk3d <faruk3d@hotmail.com>
2021-02-18 17:38:26 +0100

    Coffre machine

---
+++ Day_15_Coffe_machine/main.py
@@ -0,0 +1,40 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO 1. “What would you like? (espresso/latte/cappuccino):”
+# TODO 2. Turn off the Coffee Machine by entering “off” to the prompt
+# TODO 3. Print report
+# TODO 4. Check resources sufficient
+# TODO 5. Process coins
+# TODO 6. Check transaction successful
+# TODO 7. Make coffe
+
