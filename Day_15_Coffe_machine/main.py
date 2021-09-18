78bef28dfd3592370c25a1598cefc13d0dfa5cc2
faruk3d <faruk3d@hotmail.com>
2021-09-18 23:28:03 +0200

    coffee machine project

--- Day_15_Coffe_machine/main.py
+++ Day_15_Coffe_machine/main.py
@@ -2,6 +2,7 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -30,7 +31,43 @@ resources = {
     "coffee": 100,
 }
 
+
+def check_resources():
+    if resources['water'] < MENU[user_choice]['ingredients']['water']:
+        print('sorry')
+        user_choice
+    elif resources['milk'] < MENU[user_choice]['ingredients']['milk']:
+        print('sorry')
+        user_choice
+    elif resources['coffee'] < MENU[user_choice]['ingredients']['coffee']:
+        print('sorry')
+        user_choice
+
+bank = []
+
+report = f"water: {resources['water']}\n milk : {resources['milk']}\n coffee : {resources['coffee']}\n money : {bank} €"
 # TODO 1. “What would you like? (espresso/latte/cappuccino):”
+
+
+coffee_list = ["espresso", "latte", "cappuccino"]
+user_choice = input(str("What would you like? (espresso / latte / cappuccino):\n"))
+if user_choice in coffee_list:
+    print(f"Espresso : {MENU['espresso']['cost']} €, Latte : {MENU['latte']['cost']} €, Cappuccino : {MENU['cappuccino']['cost']} €. Please insert coins.")
+    check_resources()
+    cent_50 = input("how many 50 cent? ")
+    cent_20 = input("how many 20 cent? ")
+    cent_10 = input("how many 10 cent? ")
+    user_coins = int(cent_50) * 0.5 + int(cent_20) * 0.2 + int(cent_10) * 0.1
+    bank.append(user_coins)
+    resources.update(water =  resources['water'] - MENU[user_choice]['ingredients']['water'], milk = resources['milk'] - MENU[user_choice]['ingredients']['milk'], coffee = resources['coffee'] - MENU[user_choice]['ingredients']['coffee'])
+    print(resources)
+
+elif user_choice == "report":
+    print(report)
+else:
+    print("Wrong input, DUDE")
+
+
 # TODO 2. Turn off the Coffee Machine by entering “off” to the prompt
 # TODO 3. Print report
 # TODO 4. Check resources sufficient
