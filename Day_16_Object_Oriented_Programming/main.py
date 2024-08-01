c73cb2701682c50ac1e40a2d6e8b1bf722ff7b4b
faruk3d <faruklitip@gmail.com>
2024-08-01 21:12:35 +0200

    Turtle test

--- Day_16_Object_Oriented_Programming/main.py
+++ Day_16_Object_Oriented_Programming/main.py
@@ -1,2 +1,12 @@
-class zoo():
-    
+from turtle import Turtle, Screen
+
+# timmy is an object
+timmy = Turtle()
+print(timmy)
+timmy.color("DarkOrchid2")
+timmy.shape("turtle")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
