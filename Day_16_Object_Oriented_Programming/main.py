1c81e089f0417ac88d224ca9ffaa8b0d7c732b51
faruk3d <faruklitip@gmail.com>
2024-08-05 18:13:36 +0200

    Coffe_machine OOP

--- Day_16_Object_Oriented_Programming/main.py
+++ Day_16_Object_Oriented_Programming/main.py
@@ -1,12 +1,3 @@
-from turtle import Turtle, Screen
-
-# timmy is an object
-timmy = Turtle()
-print(timmy)
-timmy.color("DarkOrchid2")
-timmy.shape("turtle")
-timmy.forward(100)
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
