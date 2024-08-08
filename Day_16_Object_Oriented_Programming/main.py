4b0c955f8f034163728398534c843343ce089744
faruk3d <faruklitip@gmail.com>
2024-08-08 21:52:18 +0200

    check resource

--- Day_16_Object_Oriented_Programming/main.py
+++ Day_16_Object_Oriented_Programming/main.py
@@ -2,4 +2,20 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-CoffeeMaker.report
+machine_is_on = True
+money_machine = MoneyMachine()
+coffe_maker = CoffeeMaker()
+menu = Menu()
+# menu_item = MenuItem()
+
+while machine_is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        machine_is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffe_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(coffe_maker.is_resource_sufficient(drink))
