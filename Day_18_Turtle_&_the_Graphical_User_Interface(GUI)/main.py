bd653402eae1c43649eab138c19fba291b1db58f
faruk3d <faruklitip@gmail.com>
2024-08-14 18:05:36 +0200

    play with the turtle

---
+++ Day_18_Turtle_&_the_Graphical_User_Interface(GUI)/main.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("circle")
+tim.color("black")
+
+# Draw a square
+
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
