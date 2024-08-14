add3b4874d6a78df1220e429436964a9af65b396
faruk3d <faruklitip@gmail.com>
2024-08-14 20:35:15 +0200

    added version 2

--- Day_18_Turtle_&_the_Graphical_User_Interface(GUI)/main.py
+++ Day_18_Turtle_&_the_Graphical_User_Interface(GUI)/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random as r
 
 tim = Turtle()
 # Draw a square
@@ -14,17 +15,29 @@ tim = Turtle()
 #     tim.pendown()
 
 # Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon
-my_list = [{"num_side": 3, "color": "dark turquoise"}, {"num_side": 4, "color": "green yellow"}, {"num_side": 5, "color": "gold"}, {
-    "num_side": 6, "color": "sienna"}, {"num_side": 7, "color": "tomato"}, {"num_side": 8, "color": "hot pink"}, {"num_side": 9, "color": "medium orchid"}, {"num_side": 10, "color": "slate blue"}]
+# Version 1
+# my_list = [{"num_side": 3, "colour": "dark turquoise"}, {"num_side": 4, "colour": "green yellow"}, {"num_side": 5, "colour": "gold"}, {
+#     "num_side": 6, "colour": "sienna"}, {"num_side": 7, "colour": "tomato"}, {"num_side": 8, "colour": "hot pink"}, {"num_side": 9, "colour": "medium orchid"}, {"num_side": 10, "colour": "slate blue"}]
 
-for item in my_list:
-    angle = 360 / item.get("num_side")
-    color = item.get("color")
-    for l in range(item.get("num_side")):
-        tim.color(color)
-        tim.left(angle)
+# for item in my_list:
+#     angle = 360 / item.get("num_side")
+#     colour = item.get("colour")
+#     for l in range(item.get("num_side")):
+#         tim.colour(colour)
+#         tim.left(angle)
+#         tim.forward(100)
+
+# Version 2
+colours = ["dark turquoise", "green yellow", "gold", "sienna", "tomato", "hot pink", "medium orchid", "slate blue"]
+def draw_shape(num_side):
+    angle = 360 / num_side
+    for _ in range(num_side):
+        tim.right(angle)
         tim.forward(100)
 
+for shape_side in range(3, 11):
+    tim.color(r.choice(colours))
+    draw_shape(shape_side)
 
 screen = Screen()
 screen.exitonclick()
