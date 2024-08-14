0e7397f6698f83cd8dfd769d12bdfb7f4d7ac1db
faruk3d <faruklitip@gmail.com>
2024-08-14 20:26:00 +0200

    task 3

--- Day_18_Turtle_&_the_Graphical_User_Interface(GUI)/main.py
+++ Day_18_Turtle_&_the_Graphical_User_Interface(GUI)/main.py
@@ -1,32 +1,29 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
-tim.shape("circle")
-tim.color("black")
-
 # Draw a square
 # for _ in range(4):
 #     tim.forward(100)
 #     tim.right(90)
 
 # Draw a dashed line
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+# Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon
+my_list = [{"num_side": 3, "color": "dark turquoise"}, {"num_side": 4, "color": "green yellow"}, {"num_side": 5, "color": "gold"}, {
+    "num_side": 6, "color": "sienna"}, {"num_side": 7, "color": "tomato"}, {"num_side": 8, "color": "hot pink"}, {"num_side": 9, "color": "medium orchid"}, {"num_side": 10, "color": "slate blue"}]
 
-
-
-
-
-
-
-
-
-
-
+for item in my_list:
+    angle = 360 / item.get("num_side")
+    color = item.get("color")
+    for l in range(item.get("num_side")):
+        tim.color(color)
+        tim.left(angle)
+        tim.forward(100)
 
 
 screen = Screen()
