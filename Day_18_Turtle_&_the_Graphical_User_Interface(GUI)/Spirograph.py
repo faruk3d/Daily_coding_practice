414d2967d1bf93e6c12d2cf6cdb7976294cedf1a
faruk3d <faruklitip@gmail.com>
2024-08-15 18:06:18 +0200

    draw spirograph

--- Day_18_Turtle_&_the_Graphical_User_Interface(GUI)/Spirograph.py
+++ Day_18_Turtle_&_the_Graphical_User_Interface(GUI)/Spirograph.py
@@ -3,9 +3,7 @@ import turtle as t
 
 tim = t.Turtle()
 t.colormode(255)
-tim.shape("circle")
 tim.speed("fastest")
-tim.pensize(20)
 directions = [0, 90, 180, 270]
 
 def random_color():
@@ -14,7 +12,10 @@ def random_color():
     b = random.choice(range(256))
     return (r, g, b)
 
-for _ in range(300):
-    tim.pencolor(random_color())
-    tim.setheading(random.choice(directions))
-    tim.forward(30)
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+        
+draw_spirograph(5)
