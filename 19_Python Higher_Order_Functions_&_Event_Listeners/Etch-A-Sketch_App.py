7c566b0d772debc0c786c162ccbe8a75515f6d9e
faruk3d <faruklitip@gmail.com>
2024-08-26 21:27:13 +0200

    + clear()

--- 19_Python Higher_Order_Functions_&_Event_Listeners/Etch-A-Sketch_App.py
+++ 19_Python Higher_Order_Functions_&_Event_Listeners/Etch-A-Sketch_App.py
@@ -9,15 +9,22 @@ def move_forwards():
 def move_backwards():
     tim.backward(10)
 
-def move_counter():
+def turn_left():
     tim.left(45)
     
-def move_clockwise():
+def turn_right():
     tim.right(45)
+    
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
     
 screen.listen()
 screen.onkey(key = 'w', fun = move_forwards)
 screen.onkey(key = 's', fun = move_backwards)
-screen.onkey(key = 'a', fun = move_counter)
-screen.onkey(key = 'd', fun = move_clockwise)
+screen.onkey(key = 'a', fun = turn_left)
+screen.onkey(key = 'd', fun = turn_right)
+screen.onkey(key = 'c', fun = clear)
 screen.exitonclick()
