62e3edec277a4f8723ab8d87987a1c99df82889d
faruk3d <faruklitip@gmail.com>
2024-08-31 23:11:57 +0200

    [FEATURES] create snake class

--- 20_Build_the_Snake_Game_Part_1_Animation_&_Coordinates/main.py
+++ 20_Build_the_Snake_Game_Part_1_Animation_&_Coordinates/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,37 +8,20 @@ screen.tracer(0)
 screen.bgcolor("black")
 screen.title(titlestring = "Metal Gear Solide Snake")
 
-
 # Create a snake body
-statring_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
+snake = Snake()
 
-for position in statring_positions:
-    new_segment = Turtle(shape = "square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
 
 # Move the snake
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-
-        
-        
-        
     
-        
-
-
+    snake.move()
 # Control the snake
+
+
 # Detect collision with food
 # Create a scoreboard
 # Detect collision with wall
