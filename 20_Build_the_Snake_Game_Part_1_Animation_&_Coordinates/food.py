2abe8fae1cd17a36a5397350df87c13521fbf147
faruk3d <faruklitip@gmail.com>
2024-09-03 22:49:26 +0200

    [FEATURE] dev food

--- 20_Build_the_Snake_Game_Part_1_Animation_&_Coordinates/food.py
+++ 20_Build_the_Snake_Game_Part_1_Animation_&_Coordinates/food.py
@@ -2,5 +2,11 @@ from turtle import Turtle
 
 class Food:
     def __init__(self) -> None:
-        pass
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
     
