3a1bb963432a2c6d288c27faaa93c6dca90ca2d8
faruk3d <faruklitip@gmail.com>
2024-09-11 19:03:18 +0200

    class inheritance example

---
+++ 20_&_21_Build_the_Snake_Game_Part_1_Animation_&_Coordinates/class_inheritance.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self) -> None:
+        self.num_eyes = 2
+        
+    def breathe(self):
+        print("Inhale, exhale.")
+        
+class Fish(Animal):
+    def __init__(self) -> None:
+        super().__init__()
+        
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater.")
+
+    def swim(self):
+        print("Moving in water.")
+        
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
