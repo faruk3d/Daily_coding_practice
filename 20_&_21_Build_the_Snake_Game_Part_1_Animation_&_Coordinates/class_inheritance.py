f0a03469deabcf4e3170b584f305c851f33e06ec
faruk3d <faruklitip@gmail.com>
2024-09-11 19:13:44 +0200

    add quiz

--- 20_&_21_Build_the_Snake_Game_Part_1_Animation_&_Coordinates/class_inheritance.py
+++ 20_&_21_Build_the_Snake_Game_Part_1_Animation_&_Coordinates/class_inheritance.py
@@ -20,3 +20,24 @@ nemo = Fish()
 nemo.swim()
 nemo.breathe()
 print(nemo.num_eyes)
+
+
+# Quiz
+# class Dog:
+#     def __init__(self) -> None:
+#         self.temperament = "loyal"
+    
+#     def bark(self):
+#         print("Woof, woof!")
+        
+# class Labrador(Dog):
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self.is_a_good_boy = True
+        
+#     def bark(self):
+#         super().bark()
+#         print("Greetings, good sir. How do you do?")
+        
+# sparky = Labrador()
+# sparky.bark()
