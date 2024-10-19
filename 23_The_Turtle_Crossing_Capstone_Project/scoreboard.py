a846bf215c3d2f32701cc5b47efa2a85b93b22f7
faruk3d <faruklitip@gmail.com>
2024-10-19 21:12:47 +0200

    [FEATURES] add scoreboard

--- 23_The_Turtle_Crossing_Capstone_Project/scoreboard.py
+++ 23_The_Turtle_Crossing_Capstone_Project/scoreboard.py
@@ -1,5 +1,15 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
 
 
-class Scoreboard:
-    pass
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
