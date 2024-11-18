f56df776eabe79440bc61e60076d8f5ffcb548e1
faruk3d <faruklitip@gmail.com>
2024-11-18 20:57:32 +0100

    [FEATURES] final version

--- 23_The_Turtle_Crossing_Capstone_Project/main.py
+++ 23_The_Turtle_Crossing_Capstone_Project/main.py
@@ -10,6 +10,8 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(player.go_up, "Up")
 
@@ -25,11 +27,13 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
             
     # detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
             
             
             
