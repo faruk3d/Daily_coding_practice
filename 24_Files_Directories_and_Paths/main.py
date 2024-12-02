a30bfb8143b593b1c8f7feafeb4dfbb690eebae6
faruk3d <faruklitip@gmail.com>
2024-12-02 21:56:24 +0100

    [FEATURES] new version

--- 24_Files_Directories_and_Paths/main.py
+++ 24_Files_Directories_and_Paths/main.py
@@ -5,49 +5,46 @@ from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width = 600, height = 600)
-screen.tracer(0)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title(titlestring = "Metal Gear Solide Snake")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-# Create a snake body
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-# Control the snake
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-# Move the snake
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    # Detect collision with food
+
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        # Create a scoreboard
         scoreboard.increase_score()
-        
-    # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
-    
-    # Detect collision with tail
-    # for segment in snake.segments:
-    #     if segment == snake.head:
-    #         pass
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
+
 
 
 
