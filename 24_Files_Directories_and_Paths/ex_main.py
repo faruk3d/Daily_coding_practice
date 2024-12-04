d982601fb0e8263313f21866b3e8bc62577c4b16
faruk3d <faruklitip@gmail.com>
2024-12-04 22:27:12 +0100

    [FEATURES] read and write file test

--- 24_Files_Directories_and_Paths/ex_main.py
+++ 24_Files_Directories_and_Paths/ex_main.py
@@ -3,6 +3,6 @@
 #     content = file.read()
 #     print(content)
 
-# write in a file
+# write in a file, mode: w, r, a
 with open("ex_my_file.txt", mode = "a") as file:
     file.write("\nNew text.")
