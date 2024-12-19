287692572a4b9a1e20fc7fe76807e20040b94065
faruk3d <faruklitip@gmail.com>
2024-12-19 21:00:04 +0100

    PEP8

--- 24_Files_Directories_and_Paths/Mail_merge/main.py
+++ 24_Files_Directories_and_Paths/Mail_merge/main.py
@@ -1,17 +1,17 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-        
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
 with open("Input/Names/invited_names.txt") as names:
     name = names.readlines()
     for i in name:
         with open("Input/Letters/starting_letter.txt") as letters:
             letter = letters.read()
             mail = letter.replace("[name]", i.strip())
-            with open(f"./Output/ReadyToSend/letter_for_{i.strip()}.txt", mode = "a") as mails:
+            with open(f"./Output/ReadyToSend/letter_for_{i.strip()}.txt", mode="a") as mails:
                 mails.write(mail)
