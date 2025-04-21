fbebaf1f3c8be5a7ab4317fe025fdc399beb0f57
faruk3d <faruklitip@gmail.com>
2025-04-21 18:02:53 +0200

    pandas

--- 25_Working_with_CSV_data_and_Pandas/main.py
+++ 25_Working_with_CSV_data_and_Pandas/main.py
@@ -2,13 +2,20 @@
 #     data = data_file.readlines()
 #     print(data)
     
-import csv
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(row[1])
+# import csv
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
             
-    print(temperatures)
+#     print(temperatures)
+
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+
+print(type(data))
+
     
